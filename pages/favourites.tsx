55d0d7997caa5f1458edea236efcc419909d1227
Denis <denis.lopatinski@incode-it.com>
2022-01-14 17:16:12 +0200

    Push optimizate version
    Add other changes

--- pages/favourites.tsx
+++ pages/favourites.tsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import s from '../styles/favourites.module.css'
 import BlockFilm from '../components/BlockFilm.tsx'
-import Link from 'next/link'
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const [favouriteFilms, setFavouriteFilms] = useState([]);
 
+  const router = useRouter();
+  const lastPage = () => router.back();
+
   useEffect(() => {
-    if(JSON.parse(localStorage.getItem("markedFilms")) !== "[]" && localStorage.getItem("markedFilms")){
-      JSON.parse(localStorage.getItem("markedFilms")).forEach(el => {
+    const getArrFromLocal = JSON.parse(localStorage.getItem("markedFilms"))
+    if(getArrFromLocal !== "[]" && localStorage.getItem("markedFilms")){
+      getArrFromLocal.forEach(el => {
         fetch(`https://api.themoviedb.org/3/movie/${el}?api_key=82317c5aa540a6308c278894685205da&language=en-US`)
         .then(data => data.json())
         .then(json => {
@@ -20,9 +24,7 @@ export default function Home() {
 
   return (
     <div className={s.container}>
-      <Link href="/">
-        <a className={s.home}>&#8735;</a>
-      </Link>
+      <p onClick={lastPage} className={s.home}>&#8735;</p>
       <h2>Your Favourite</h2>
       <div className={s.blocks}>
         {
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,60 +3,42 @@ import s from '../styles/Home.module.css'
 import BlockFilm from '../components/BlockFilm.tsx'
 import Filter from '../components/Filter'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Home() {
-
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(1);
   const [currentFilms, setCurrentFilms] = useState([]);
-
   const [filter, setFlter] = useState<string>("descending");
   const changeFilterHandler = (str: string) => setFlter(str); 
 
 
-  const fetchFilms = (currentFilter: string) => {
-    fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=82317c5aa540a6308c278894685205da&language=en-US&page=
-    ${currentFilter === "descending" ? currentPage : 26 - currentPage}`)
-      .then(data => data.json())
-      .then(json => {
-        setCurrentFilms(json.results)
-    })
+const fetchFilms = (currentFilter: string) => {
+  const checkFilter = currentFilter === "descending" ? currentPage : 26 - currentPage;
+  fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=82317c5aa540a6308c278894685205da&language=en-US&page=${checkFilter}`)
+    .then(data => data.json())
+    .then(json => {
+      setCurrentFilms(json.results)
+  })
   }
+   
   useEffect(() => {
-    if (/\?page\=\d+/.test(window.location.href)) {
-      let thisPage: number = Number(window.location.href.match(/(?<=page=)\d+/)[0]);
-      if(thisPage > 25){
-        thisPage = 25
-        let baseUrl = window.location.href.split("?")[0];
-        setTimeout(() => {
-          window.history.pushState('name', '', baseUrl + `?page=25`);
-        },200)
-      }
-      setCurrentPage(thisPage);
-    }else{
-      fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=82317c5aa540a6308c278894685205da&language=en-US&page=1")
-      .then(data => data.json())
-      .then(json => {
-        setCurrentFilms(json.results)
-      })
+    if(!router.isReady) return
+    const queryPage = router.query.page;
+    if(Number(queryPage) < 26){
+      queryPage && setCurrentPage(Number(queryPage))
+    }else if(Number(queryPage) > 25){
+      setCurrentPage(25)
     }
-  }, [])
+  }, [router.isReady])
 
   useEffect(() => {
     fetchFilms(filter);
-    let baseUrl = window.location.href.split("?")[0];
-    window.history.pushState('name', '', baseUrl + `?page=${currentPage}`);
+    router.push(`/?page=${currentPage}`)
   }, [currentPage, filter])
 
-  const prevPage = () => {
-    if(currentPage > 1){
-      setCurrentPage(currentPage - 1)
-    }
-  } 
-  const nextPage = () => {
-    if(currentPage <= 24){
-      setCurrentPage(old => old + 1)
-    }
-  } 
+  const prevPage = () =>  currentPage > 1 && setCurrentPage(currentPage - 1);
+  const nextPage = () => currentPage <= 24 && setCurrentPage(currentPage + 1);
   
   return (
     <div className={s.container}>
