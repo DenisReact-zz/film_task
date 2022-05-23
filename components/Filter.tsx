863bfe393ee6583621af5cb5bb56417f509dd729
DenisReact <den.lopatinski.dev@gmail.com>
2022-05-23 17:31:12 +0300

    Add tests for project, create settings for react-testing-library

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import s from '../styles/Filter.module.css'
+import React, { FC } from "react";
+import s from "../styles/Filter.module.css";
 
-const Filter = (props) => {
-    const thumb = React.useRef(null);
-
-    const click_button = (filter:string, index: number) => () => {
-        props.onChangeFilter(filter);
-        thumb.current.style.left = `${50 * index}%`
-    }
-    return (
-        <div className={s.filter}>
-            <div className={s.blocks}>
-                <button onClick={click_button("descending", 0)}>Descending</button>
-                <button onClick={click_button("ascending", 1)}>Ascending</button>
-                <div className={s.thumb} ref={thumb}></div>
-            </div>
-        </div>
-    )
+interface PropsFilter {
+  onChangeFilter: (str: string) => void;
 }
+const Filter: FC<PropsFilter> = ({ onChangeFilter }) => {
+  const thumb = React.useRef(null);
 
-export default Filter
+  const click_button = (filter: string, index: number) => () => {
+    onChangeFilter(filter);
+    thumb.current.style.left = `${50 * index}%`;
+  };
+  return (
+    <div className={s.filter} data-testid="filters">
+      <div className={s.blocks}>
+        <button
+          onClick={click_button("descending", 0)}
+          data-testid="buttonOfFilter"
+        >
+          Descending
+        </button>
+        <button
+          onClick={click_button("ascending", 1)}
+          data-testid="buttonOfFilter"
+        >
+          Ascending
+        </button>
+        <div className={s.thumb} ref={thumb}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import s from '../styles/Home.module.css'
-import BlockFilm from '../components/BlockFilm'
-import Filter from '../components/Filter'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import React, { useState, useEffect } from "react";
+import s from "../styles/Home.module.css";
+import BlockFilm from "../components/BlockFilm";
+import Filter from "../components/Filter";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
   const [currentPage, setCurrentPage] = useState(1);
   const [currentFilms, setCurrentFilms] = useState([]);
   const [filter, setFlter] = useState<string>("descending");
-  const changeFilterHandler = (str: string) => setFlter(str); 
+  const changeFilterHandler = (str: string) => setFlter(str);
 
-
-const fetchFilms = (currentFilter: string) => {
-  const checkFilter = currentFilter === "descending" ? currentPage : 26 - currentPage;
-  fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=82317c5aa540a6308c278894685205da&language=en-US&page=${checkFilter}`)
-    .then(data => data.json())
-    .then(json => {
-      setCurrentFilms(json.results)
-  })
-  }
-   
   useEffect(() => {
-    if(!router.isReady) return
+    if (!router?.isReady) return;
     const queryPage = router.query.page;
-    if(Number(queryPage) < 26){
-      queryPage && setCurrentPage(Number(queryPage))
-    }else if(Number(queryPage) > 25){
-      setCurrentPage(25)
+    if (Number(queryPage) < 26) {
+      queryPage && setCurrentPage(Number(queryPage));
+    } else if (Number(queryPage) > 25) {
+      setCurrentPage(25);
     }
-  }, [router.isReady])
+  }, [router?.isReady]);
 
   useEffect(() => {
-    fetchFilms(filter);
-    router.push(`/?page=${currentPage}`)
-  }, [currentPage, filter])
+    let cleanupFunction = false;
+    const fetchFilms = (currentFilter: string) => {
+      const checkFilter =
+        currentFilter === "descending" ? currentPage : 26 - currentPage;
+      fetch(
+        `https://api.themoviedb.org/3/movie/top_rated?api_key=82317c5aa540a6308c278894685205da&language=en-US&page=${checkFilter}`
+      )
+        .then((data) => data.json())
+        .then((json) => {
+          if (!cleanupFunction) setCurrentFilms(json?.results);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
 
-  const prevPage = () =>  currentPage > 1 && setCurrentPage(currentPage - 1);
+    fetchFilms(filter);
+    router?.push && router.push(`/?page=${currentPage}`);
+    return () => {
+      cleanupFunction = true;
+    };
+  }, [currentPage, filter]);
+
+  const prevPage = () => currentPage > 1 && setCurrentPage(currentPage - 1);
   const nextPage = () => currentPage <= 24 && setCurrentPage(currentPage + 1);
-  
+
   return (
-    <div className={s.container}>
+    <div className={s.container} data-testid="home">
       <Link href="/favourites">
         <a className={s.favourite}>
           <img src="/images/star.svg" alt="" />
@@ -50,15 +59,19 @@ const fetchFilms = (currentFilter: string) => {
       <h2>Top 500 films on TMDB</h2>
       <Filter onChangeFilter={changeFilterHandler} />
       <div className={s.blocks}>
-        {
-          currentFilms.map(el => <BlockFilm key={el.id} dataFilm={el} />)
-        }
+        {currentFilms.map((el) => (
+          <BlockFilm key={el.id} dataFilm={el} />
+        ))}
       </div>
       <div className={s.navigation}>
-          <button onClick={prevPage} className={s.prev}>&#8735;</button>
-            <p>{currentPage}</p>
-          <button onClick={nextPage} className={s.next}>&#8735;</button>
-        </div>
+        <button onClick={prevPage} className={s.prev} data-testid="prevArrow">
+          &#8735;
+        </button>
+        <p data-testid="currentPage">{currentPage}</p>
+        <button onClick={nextPage} className={s.next} data-testid="nextArrow">
+          &#8735;
+        </button>
+      </div>
     </div>
-  )
+  );
 }
